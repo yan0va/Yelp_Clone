48742dd02804468fbf85aa9faff2960f85fa7659
Anna <anna.yanova@gmail.com>
2014-04-11 14:04:11 +0100

    Created form to be able to add restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,16 @@
 class RestaurantsController < ApplicationController
 
   def index
-   
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    Restaurant.create params[:restaurant].permit(:name, :location, :category)
+    redirect_to '/restaurants'
   end
 
 end
--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -9,6 +9,22 @@ describe "the restaurants index page" do
       expect(page).to have_content("No restaurants added")
     end
 
+    context "adding a restaurant" do
+
+      it "should be listed on the index" do
+        visit '/restaurants'
+        click_link "Add a restaurant"
+        fill_in "Name", with: "Ozone Coffee"
+        fill_in "Category", with: "Breakfast & Brunch"
+        fill_in "Location", with: "Shoreditch, East London"
+        click_button "Create Restaurant"
+
+        expect(current_path).to eq '/restaurants'
+        expect(page).to have_content "Ozone Coffee"
+      end
+
+    end
+
   end
 
 end
